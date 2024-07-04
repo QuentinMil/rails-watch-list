52e2e69ebfcb8f003b48a35b0528559208dddb77
(Quentin Milhomme) <milhommequentin@yahoo.fr>
2024-07-04 17:00:48 +0200

    exercice 2 MAJ

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  Rails.application.routes.draw do
+    resources :lists, only: [:index, :show, :new, :create]
+  end
 end
